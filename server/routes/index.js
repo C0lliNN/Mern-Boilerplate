da04e1d38a3349d7dd8477ffc5130f356c086c34
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-11-06 13:39:33 -0300

    Create Commands and Update Routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const users = require('./users');
 const todos = require('./todos');
 const error = require('../middlewares/error');
@@ -8,6 +9,17 @@ const router = express.Router();
 router.use(express.json());
 router.use('/api/v1', users);
 router.use('/api/v1/todos', todos);
+
+if (process.env.NODE_ENV === 'production') {
+  router.use(express.static('../client/build'));
+
+  router.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
+    );
+  });
+}
+
 router.use(error);
 
 module.exports = router;
