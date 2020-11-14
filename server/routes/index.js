733435c661c33bd131f7274f6b1a0901fc0aa994
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-11-14 15:57:37 -0300

    Add Compression and Helmet Middlewares

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 const users = require('./users');
 const todos = require('./todos');
 const error = require('../middlewares/error');
@@ -11,7 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
   router.use(cors());
 }
 
+router.use(compression());
+router.use(helmet({ contentSecurityPolicy: false }));
 router.use(express.json());
+
 router.use('/api/v1', users);
 router.use('/api/v1/todos', todos);
 
